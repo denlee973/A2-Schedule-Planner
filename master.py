df9024fe518ef5f739587c7df03e5623e518a915
Denny <denlee973@users.noreply.github.com>
2017-10-20 20:34:20 -0400

    stuff
    I always don't update this enough... kind of a pain tbh

--- master.py
+++ master.py
@@ -2,6 +2,8 @@
 # master.py
 # Created 19 October 2017
 # Schedule Planner for CS 12 A2
+import datetime
+
 
 def update_data():
     f = open('schedule.txt')
@@ -10,38 +12,37 @@ def update_data():
     return data
 data = update_data()
 
-def tryexc(value,do,wrong):
+def tryexc(do):
     trying = True
     while trying:
         try:
-            if value == "int" or value == "float":
-                answer = input(do)
-            elif value == "str":
-                answer = raw_input(do)
+            answer = input(do)
         except:
-            print wrong
+            print "Please input a numerical value."
         else:
-            trying = False
+            if answer > 25:
+                print "Please input a value less than 25."
+            else:
+                trying = False
     return answer
+
+def date():
+    from datetime import date
+    import calendar
+    now = datetime.datetime.now()
+    print "{}, {} {}, {}".format(calendar.day_name[date.today().weekday()],calendar.month_name[now.month],now.day,now.year)
+
+
 def school_blocks():
     school = raw_input("What school do you go to? ")
     if school != "Havergal" and school != "Havergal College" and school != "havergal" and school != "havergal college" and school != "HC" and school != "hc":
-        days = tryexc("int","How many days are in your schedule? ","Please input a numerical value.")
-        while days > 50:
-            print "Please input a value less than 50."
-            days = tryexc("int","How many days are in your schedule? ","Please input a numerical value.")
+        days = tryexc("How many days are in your schedule? ")
         print days
         
-        periods = tryexc("int","How many periods are in a day? ","Please input a numerical value.")
-        while periods > 50:
-            print "Please input a value less than 50."
-            periods = tryexc("int","How many periods are in a day? (If it is not evenly divided by time, please input the common denominator of times.) ","Please input a numerical value.")
+        periods = tryexc("How many periods are in a day? (If it is not evenly divided by time, please input the common denominator of times.)")
         print periods
         
-        blocks = tryexc("int","How many courses do you have? ","Please input a numerical value.")
-        while blocks > 50:
-            print "Please input a value less than 50."
-            blocks = tryexc("int","How many courses do you have? ","Please input a numerical value.")
+        blocks = tryexc("How many courses do you have? ")
         print blocks
 
     else:
@@ -67,7 +68,6 @@ def in_schedule(school, days, blocks, periods):
 
     f.close()
 def main(data):
-    print data
     if len(data) >= 8:
         print "Schedule found."
         info = data[len(data)-1].split('\t')
@@ -88,24 +88,26 @@ def main(data):
     print ""
     print school
     if school == "Havergal":
-        a = data[0]
-        b = data[1]
-        c = data[2]
-        d = data[3]
-        e = data[4]
-        f = data[5]
-        g = data[6]
-        h = data[7]
+        a = data[0][0:len(data[0])-1]
+        b = data[1][0:len(data[1])-1]
+        c = data[2][0:len(data[2])-1]
+        d = data[3][0:len(data[3])-1]
+        e = data[4][0:len(data[4])-1]
+        f = data[5][0:len(data[5])-1]
+        g = data[6][0:len(data[6])-1]
+        h = data[7][0:len(data[7])-1]
         tabs = [1,1,1,1,1,1,1,1]
-        # for e in range(8):
-        #     if len(data[e]) > 8:
-        #         tabs[e] += 1
+        for e in range(8):
+            if len(data[e]) > 8:
+                tabs[e] += 1
+        print tabs
         schedule = [[a,b,c,d],[e,f,g,h],[c,d,a,b],[g,h,e,f],[b,a,d,c],[f,e,h,g],[d,c,b,a],[h,g,f,e]]
         print "Day 1\tDay 2\tDay 3\tDay 4\tDay 5\tDay 6\tDay 7\tDay 8"
         for j in range(4):
             for k in range(8):
-                course = schedule[k][j]
-                print course[0:len(course)-1]+'\t',
+                print str(schedule[k][j]) +'\t',
             print ""
+        print ""
 
 main(data)
+date()
